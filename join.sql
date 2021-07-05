710e307f398ef116e949b96b6183b6019046e614
Cargalher <cargalher@gmail.com>
2021-07-05 17:40:20 +0200

    Query ex.3- JOIN

--- join.sql
+++ join.sql
@@ -37,15 +37,22 @@ JOINS:
     ON students.degree_id = degree.id
     WHERE degrees.name = 'Corso di Laurea in Economia'
 
-2- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+2- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze (14 CORSI)
 
     SELECT courses.id, courses.name, courses.period, courses.year, courses.cfu, courses.website
     FROM courses
     JOIN departments
     ON courses.degree_id = departments.id
     WHERE departments.name = 'Dipartimento di Neuroscienze'
-    
-3- Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+3- Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44) (18 CORSI)
+
+    SELECT courses.id, courses.name, courses.period, courses.year, courses.cfu, courses.website
+    FROM courses
+    JOIN teachers
+    ON courses.degree_id = teachers.id
+    WHERE teachers.id = 44
+
 4- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 5- Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 6- Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
